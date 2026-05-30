8d7ad5f0d749b3aeee01f9b4a9da16edc60e2f91
dx-bear <dxbear@protonmail.com>
2026-05-30 23:22:49 +0300

    Refactor main.py to improve browser context handling and update uv.lock to version 1.0.4.3

--- main.py
+++ main.py
@@ -4,26 +4,22 @@ from browserBear.browsers import StealthBrowser
 
 async def main():
 
-    # Start new browser with new fingerprint and connect to it
-    # The browser might take less than a second to warm up.
-    # warm up process is very important to avoid anti-bot measures, as it mimics human behavior.
-    page = await StealthBrowser.connect()
+    # Launch a new browser context with one page
+    b = await StealthBrowser.lunch_context()
+    page = b.pages[0]
 
-    # safly navigate to a page, without triggering anti-bot measures
-    await page.safe_goto("https://example.com")
+    # Safely navigate to a page, without triggering anti-bot measures
+    await StealthBrowser.safe_goto(b, "https://example.com")
 
-    # wait a second
-    await asyncio.sleep(1)
+    # Wait a second, increase this if you want to see the page before solving the challenge
+    await asyncio.sleep(5)
 
-    # print page title
-    title = await page.title()
-    print(f"Page title: {title}")
+    # Save full screenshot of the page
+    await page.screenshot(path="screenshot.png", full_page=True)
 
-    # safly clean page in the background
-    await page.clean()
+    # Close the browser context and free resources
+    await StealthBrowser.shutdown(b)
 
-    # disconnect and close the browser
-    await StealthBrowser.shutdown_all()
 
 if __name__ == "__main__":
     asyncio.run(main())
